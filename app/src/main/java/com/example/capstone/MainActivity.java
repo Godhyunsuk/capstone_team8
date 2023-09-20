52b3e7ef05bf7e2396eef70f7637719e9ab0b428
Hyunsuk Ju <wngustjr1306@naver.com>
2023-09-20 16:44:33 +0900

    test test

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       //최종테스트입니다.
+       //최종테스트입니다2
     }
 }
