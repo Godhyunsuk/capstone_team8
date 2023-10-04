e89f8d32ac8333d24faebd2e08f9a06cd8e1d981
Hyunsuk Ju <wngustjr1306@naver.com>
2023-10-04 22:30:54 +0900

    로그인/회원가입 기능 구현 (파베연동)

--- app/src/main/java/com/example/capstone/LoginActivity.java
+++ app/src/main/java/com/example/capstone/LoginActivity.java
@@ -1,27 +1,39 @@
 package com.example.capstone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
-    //파베 변수 12345
-    private FirebaseAuth mAuth;
 
-    Button btnLogin, btnRegister;
+    private FirebaseAuth mFirebaseAuth; //파이어베이스 인증
+    private DatabaseReference mDatabaseRef; //실시간 데이터베이스
+    private EditText etEmail, etPwd, etPwdChk, etNickname; //입력 필드
+    private Button btnLogin, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //파베 생성
-        mAuth = FirebaseAuth.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("capstone");
+
+        etEmail = findViewById(R.id.et_email);
+        etPwd = findViewById(R.id.et_pwd);
 
         btnLogin = (Button) findViewById(R.id.btn_login);
         btnRegister = (Button) findViewById(R.id.btn_register);
@@ -30,9 +42,23 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
+                String strEmail = etEmail.getText().toString();
+                String strPwd = etPwd.getText().toString();
+
+                mFirebaseAuth.signInWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
             }
         });
         //회원가입 버튼 클릭 시
--- app/src/main/java/com/example/capstone/RegisterActivity.java
+++ app/src/main/java/com/example/capstone/RegisterActivity.java
@@ -1,24 +1,78 @@
 package com.example.capstone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import java.util.Map;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
-    Button btnPrevious;
+    private FirebaseAuth mFirebaseAuth; //파이어베이스 인증
+    private DatabaseReference mDatabaseRef; //실시간 데이터베이스
+    private EditText etEmail, etPwd, etPwdChk, etNickname; //입력 필드
+    private Button btnRegister; // 회원가입 버튼
+    private Button btnPrevious; //이전 버튼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("capstone");
+
+        etEmail = findViewById(R.id.et_email);
+        etPwd = findViewById(R.id.et_pwd);
+        etPwdChk = findViewById(R.id.et_pwdchk);
+        etNickname = findViewById(R.id.et_nickname);
+
+        btnRegister = (Button) findViewById(R.id.btn_register);
         btnPrevious = (Button) findViewById(R.id.btn_previous);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { //회원가입 처리
+                String strEmail = etEmail.getText().toString();
+                String strPwd = etPwd.getText().toString();
+                String strPwdChk = etPwdChk.getText().toString();
+                String strNickname = etNickname .getText().toString();
+
+                mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                            UserAccount account = new UserAccount();
+                            account.setIdToken(firebaseUser.getUid());
+                            account.setEmailId(firebaseUser.getEmail());
+                            account.setPassword(strPwd);
+
+                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+
+                            Toast.makeText(RegisterActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+                        }
+
+                        else {
+                            Toast.makeText(RegisterActivity.this, "회원가입 실패", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
         btnPrevious.setOnClickListener(new View.OnClickListener() {
             @Override
