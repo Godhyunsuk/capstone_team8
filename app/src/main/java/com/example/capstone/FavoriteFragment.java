9b0b2303a83016447ee2a6bfdd24dc10a412ca18
Hyunsuk Ju <wngustjr1306@naver.com>
2023-11-08 23:51:35 +0900

    커스텀 체크박스와, 즐겨찾기에 음료 등록시 즐겨찾기 페이지 변화

--- app/src/main/java/com/example/capstone/FavoriteFragment.java
+++ app/src/main/java/com/example/capstone/FavoriteFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,10 +13,18 @@ import androidx.fragment.app.Fragment;
 
 public class FavoriteFragment extends Fragment {
 
+    ListView listView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_favorite,container,false);
+
+        listView = (ListView) view.findViewById(R.id.flistView);
+
+        if(!DrinkActivity.favoriteList.isEmpty()) {
+            listView.setVisibility(View.VISIBLE);
+        }
 
         return view;
     }
