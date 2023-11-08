3a1b37cf8f23249d44175426a42c42a29cdc5959
sunjong2 <saysaysay223@gmail.com>
2023-11-08 16:57:22 +0900

    엔젤리너스 uri 출력하도록 함

--- app/src/main/java/com/example/capstone/AngelActivity.java
+++ app/src/main/java/com/example/capstone/AngelActivity.java
@@ -10,12 +10,17 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.bumptech.glide.Glide;
-import com.example.capstone.ConnectDB.SelectData;
+import com.example.capstone.DAO.SelectData;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import java.net.URI;
+import java.util.ArrayList;
 
 public class AngelActivity extends AppCompatActivity {
     SelectData sd = new SelectData("angelinus","angelinus");
@@ -32,26 +37,17 @@ public class AngelActivity extends AppCompatActivity {
         setContentView(R.layout.activity_angel);
 
         load = (ImageView)findViewById(R.id.imageView4);
-        StorageReference storages = FirebaseStorage.getInstance().getReference().child("back");
+        StorageReference storages = FirebaseStorage.getInstance().getReference().child("angelinus");
         //음료 아이디와 파이어베이스에 저장할 사진 이름을 같게할거임.
         //이건 내가 따로 저장해둔 사진 이름들이고, 나중에 음료 아이디로 바꿔줄 예정.
-        String[] args = {"4","13","15"};
+        String[] args = {"AG001","AG002","AG003"};
 
         for(String i : args){
-            LinearLayout layout = (LinearLayout) findViewById(R.id.q);
-            ImageView iv = new ImageView(AngelActivity.this);
-            iv.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            layout.addView(iv);
-            // ^^^^^^여기까진 사진이 제대로 나오는지 레이아웃으로 만들어본거임.
-
-            StorageReference storage = storages.child(i+".PNG");
-            storage.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+            StorageReference storage = storages.child(i+".png");
+            storage.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                 @Override
-                public void onComplete(@NonNull Task<Uri> task) {
-                    if (task.isSuccessful()) {
-                        //여기에서 정해진 ImageView에 값을 저장해서 보여줌.
-                        Glide.with(AngelActivity.this).load(task.getResult()).into(iv);
-                    }
+                public void onSuccess(Uri uri) {
+                    System.out.println(uri);
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
@@ -59,7 +55,8 @@ public class AngelActivity extends AppCompatActivity {
 
                 }
             });
+
         }
-        System.out.println(sd.CoffeeObject[0]);
+
     }
 }
