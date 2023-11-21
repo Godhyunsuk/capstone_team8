b52fd74378cea8828c66f80f75e15793b505e2d1
Hyunsuk Ju <wngustjr1306@naver.com>
2023-11-21 20:56:21 +0900

    로그아웃 구현

--- app/src/main/java/com/example/capstone/MyinfoFragment.java
+++ app/src/main/java/com/example/capstone/MyinfoFragment.java
@@ -1,20 +1,42 @@
 package com.example.capstone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MyinfoFragment extends Fragment {
+
+    Button logout;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_myinfo,container,false);
 
+        logout = (Button) view.findViewById(R.id.buttonlogin);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+                // 사용자 로그아웃
+                firebaseAuth.signOut();
+
+                Intent intent = new Intent(getActivity(),LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
